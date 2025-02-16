0a3f597684a628a7272680e01c5db48cafdbd941
hisayuki <syuaibsyuaib@gmail.com>
2025-02-17 06:38:55 +0800

    ok

--- js/script.js
+++ js/script.js
@@ -5,17 +5,19 @@ function scanning() {
 }
 
 function setHasil(hasil_scan) {
-  $("#pesan").text(hasil_scan);
+  log(hasil_scan)
 
-  ambilDataSiswa("hasil scan", {  nisn: hasil_scan });
+  ambilDataSiswa("hasil scan", { nisn: hasil_scan });
 }
 
-const dt = new Date()
-const bln = dt.getMonth()
-const thn = dt.getFullYear()
-const bln_thn = `${bln}/${thn}`
+const dt = new Date();
+const tgl = dt.getDate();
+const bln = dt.getMonth() + 1;
+const thn = dt.getFullYear();
+const bln_thn = `${bln}/${thn}`;
+$('#tanggal').text(`Tanggal : ${tgl}-${bln}-${thn}`)
 
-ambilDataSiswa("ambil data siswa", {bln_thn: bln_thn});
+ambilDataSiswa("ambil data siswa", { bln_thn: bln_thn });
 
 function ambilDataSiswa(flag, obj = {}) {
   $(".modal-backdrop").show();
@@ -30,26 +32,37 @@ function ambilDataSiswa(flag, obj = {}) {
     .then((res) => res.json())
     .then((res) => {
       let data = res.pesan;
-      $("#pesan").text(data);
+      log(data)
+      data.shift()
       generateTableContent(data);
       $("#loading").hide();
       $(".modal-backdrop").remove();
     })
     .catch((err) => {
-      $("#pesan").text(err);
+      log(err);
       $("#loading").modal("hide");
     });
 }
 
 function generateTableContent(data) {
   let html = "";
+  console.log(data);
   data.forEach((item, index) => {
     html += `<tr>
                 <td>${index + 1}</td>
                 <td>${item[1]}</td>
                 <td>${item[2]}</td>
-                <td>hadir</td>
+                <td>${item[tgl + 3] ? "hadir" : ""}</td>
                 </tr>`;
   });
   document.querySelector("tbody").innerHTML = html;
+}
+
+function log(data){
+    if(typeof(data) == "object"){
+        $("#pesan").text(JSON.stringify(data, null, 1));
+    }else{
+        $("#pesan").text(data);
+    }
+    hljs.highlightAll();
 }
